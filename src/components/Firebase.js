e46b559da16cb2c8521792cbb9fa857eb270bf68
Suraj Godse <godsesuraj2140@gmail.com>
2024-08-20 00:42:08 -0500

    Update Firebase.js

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -3,13 +3,13 @@ import { getDatabase } from "firebase/database";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA1_eCQqnznB_9YgLABts1UyrRc9NzBKCk",
-  authDomain: "login-auth-f1775.firebaseapp.com",
-  databaseURL: "https://login-auth-f1775-default-rtdb.firebaseio.com",
-  projectId: "login-auth-f1775",
-  storageBucket: "login-auth-f1775.appspot.com",
-  messagingSenderId: "320384479194",
-  appId: "1:320384479194:web:0363650d3620acd5149ae3"
+  apiKey: "***********",
+  authDomain: "*************",
+  databaseURL: "**************",
+  projectId: "*************",
+  storageBucket: "****************",
+  messagingSenderId: "**************",
+  appId: "*************************"
 };
 
 // Initialize Firebase
